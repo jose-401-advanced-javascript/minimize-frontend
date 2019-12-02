30d547fbb5abe86adb9a5a764444e3b87692cd4f
Jose <j.a.ojeda@live.com>
2019-12-02 15:28:51 -0800

    updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Switch,
+  Route
 } from 'react-router-dom';
 import Header from './Header';
+import LoginUser from '../containers/LoginUser';
+import SignupUser from '../containers/SignupUser';
 
 export default function App() {
   return (
     <Router>
       <Header />
+      <Switch>
+        <Route path="/login" component={LoginUser} />
+        <Route path="/signup" component={SignupUser} />
+      </Switch>
     </Router>
   );
 }
