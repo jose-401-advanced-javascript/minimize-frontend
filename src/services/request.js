189cc5ea28a365a13ac12bcf159db1b9180d8dfc
Jose <j.a.ojeda@live.com>
2019-12-02 14:55:39 -0800

    created services with auth.js and reuquest.js

---
+++ src/services/request.js
@@ -0,0 +1,21 @@
+const request = (url, method, body) => {
+  return fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: body && JSON.stringify(body)
+  })
+    .then(res => {
+      if(!res.ok) {
+        throw `Unable to fetch from ${url}`;
+      }
+
+      return res.json();
+    });
+};
+
+export const post = (url, body) => request(url, 'POST', body);
+export const get = url => request(url, 'GET');
+
